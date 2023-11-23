842327065742d51ac0a86b8815fb4b21ac030ef9
Precious <precious.tochi.ikechukwu@gmail.com>
2023-11-23 11:28:36 +0100

    page

--- video-search.js
+++ video-search.js
@@ -3,3 +3,9 @@ document.querySelector(".nav1").onclick = function(){
     this.classList.toggle("active");
     nav.classList.toggle("active");
 }
+
+
+const accessKey = "0544076e18219bd931751970012492ef"
+
+const myForm = document.getElementById("myForm");
+const myInput = document.getElementById("search-input");
